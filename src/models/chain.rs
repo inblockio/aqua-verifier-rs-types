91eb3f45c673162c80c5d558e7813066a32729a2
arthur-kamau <kamaukenn11@gmail.com>
2025-01-08 12:35:19 +0300

    fix file index json structer ie struct serde macro directive

--- src/models/chain.rs
+++ src/models/chain.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AquaChain {
+    #[serde(with = "tuple_vec_map")]
     pub file_index: Vec<(Hash, String)>,
     // #[serde_as(as = "Vec<(_, _)>")]
     // #[serde(with = "tuple_list")]
